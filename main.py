96745242dd8bdaa4238fb0223a785d0aaeaff517
rinapark19 <rinapark19@gmail.com>
2023-03-09 19:12:16 +0900

    remove self

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def twoSum(self, nums: list[int], target: int) -> list[int]:
+def twoSum(nums: list[int], target: int) -> list[int]:
     for i in range(len(nums)):
         if(i + 1 <= len(nums)):
                 if nums[i] + nums[i + 1] == target:
