994badd3df1d6a3e56c06a972e26b21ae51c1894
rinapark19 <rinapark19@gmail.com>
2023-03-09 19:17:47 +0900

    fixing typo

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def twoSum(nums: list[int], target: int) -> list[int]:
+from typing import List
+def twoSum(nums: List[int], target: int) -> List[int]:
     for i in range(len(nums)):
         if(i + 1 <= len(nums)):
                 if nums[i] + nums[i + 1] == target:
                     ans = [i, i + 1]
                     return ans
+                
+print(twoSum([1,2], 3))
